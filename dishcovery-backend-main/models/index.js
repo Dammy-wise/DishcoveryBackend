fa6807558d6883e885bcc2ac91e27d73a73a18fc
Coding <150249195+Dammy-wise@users.noreply.github.com>
2025-12-28 18:45:42 +0100

    commit8

--- dishcovery-backend-main/models/index.js
+++ dishcovery-backend-main/models/index.js
@@ -19,13 +19,6 @@ Favorite.belongsTo(User, { foreignKey: "userId" });
 Recipe.hasMany(Favorite, { foreignKey: "recipeId", onDelete: "CASCADE" });
 Favorite.belongsTo(Recipe, { foreignKey: "recipeId" });
 
-<<<<<<< HEAD
 // ❌ REMOVED: Don't sync here, do it in server.js only
-=======
-// ✅ TEMP: update DB structure
-sequelize
-  .then(() => console.log("✅ DB updated with Favorite model"))
-  .catch((err) => console.error("❌ DB sync error:", err));
->>>>>>> 94151f6a66c508549eb2e4143245477c0ed5222e
 
 export default sequelize;
